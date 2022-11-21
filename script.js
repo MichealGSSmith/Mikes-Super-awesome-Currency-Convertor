2c6057f13ca22eabe03471214dc760045a824955
MichealGSSmith <w0777761@myscc.ca>
2022-11-21 03:41:52 -0500

    html and js changes
    I added the rest of my html and then got some js uploaded, i seem to be having an issue with my api its giving me this code
    Uncaught (in promise) TypeError: NetworkError when attempting to fetch resource.
    I am not sure why i dont know if its cause i am running local host or if it is a CORS issue, or if i just made a mistake in my code :@

---
+++ script.js
@@ -0,0 +1,21 @@
+const convert = document.getElementById("convert");
+const result = document.getElementById("result");
+const yourCurrency = document.getElementById("yourCurrency");
+const wantedCurrency = document.getElementById("wantedCurrency");
+const amount = document.getElementById("amount");
+
+convert.addEventListener("click", function() {
+    let fromCurrency = yourCurrency.value;
+    let toCurrency = wantedCurrency.value;
+    let amt = amount.value;
+
+    fetch(`https://v6.exchangerate-api.com/v6/b9290a5c87aaef275a558257/latest/${fromCurrency}`)
+    .then(response => {
+        return response.json();
+    })
+    .then(data => {
+        let rate= data.rates[toCurrency];
+        let total = rate * amt;
+        result.innerHTML = `${amt} ${fromCurrency} = ${total} ${toCurrency}`;
+    });
+});
